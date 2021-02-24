095bd9c65a8884689c860bec57a892f961f5b2f6
Mrinal Kumar <Mrinal.Kumar@BritishCouncil.Org>
2021-02-24 21:39:52 +0530

    added EFCore5 setup

---
+++ EFCore5.Data/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using EFCore5.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore5.Data
+{
+    public class AppContext: DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=INDEL5BL0528\\SQLEXPRESS;initial catalog=samuraidb");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ EFCore5.UI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace EFCore5.UI
+{
+    class Program
+    {
+        private static EFCore5.Data.AppContext _context = new EFCore5.Data.AppContext();
+        static void Main(string[] args)
+        {
+           bool b = _context.Database.EnsureCreated(); // ensure db is created in server.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
